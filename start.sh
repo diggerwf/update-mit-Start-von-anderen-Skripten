0c1ed5cc637776db65bd32e8eaebfe96d4b4cc36
diggerwf <80178091+diggerwf@users.noreply.github.com>
2025-05-08 15:01:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,14 +11,18 @@ while true; do
     read -p "Deine Wahl (1 oder 2): " auswahl
 
     if [ "$auswahl" == "1" ]; then
-        echo "Programm wird beendet."
-        break
+        # Bestätigung abfragen
+        read -p "Bist du sicher? Tippe erneut 1 zum Beenden: " bestätigung
+        if [ "$bestätigung" == "1" ]; then
+            echo "Programm wird beendet."
+            break
+        else
+            echo "Abbruch. Zurück zum Menü."
+        fi
     elif [ "$auswahl" == "2" ]; then
         # Überprüfen, ob die Datei existiert
         if [ -f "$datei2" ]; then
-            # Ausführungsrechte setzen
             chmod +x "$datei2"
-            # Datei ausführen
             ./"$datei2"
         else
             echo "Datei '$datei2' wurde nicht gefunden."
